6fed517f40b20296707b42237e001e25606e3267
Bora <boraozdogan99@gmail.com>
2021-04-18 01:17:24 +0300

    stop processing the same data every time the program runs

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
+import os
 import re
 import bs4
-import nltk
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -42,30 +42,37 @@ def clean_summary(summary, contractions=_contractions_en):
     return result
 
 
-def prepare_dataset(data, max_len_text, max_len_sum, verbose=False):
-    if isinstance(data, str):
-        data = pd.read_csv(data, nrows=100000)
+def prepare_dataset(datafile, max_len_text, max_len_sum, nrows=None, verbose=False):
+    if os.path.exists(f"{datafile}-cache.gz"):
+        print("Cached data found")
+        print("Loading preprocessed file")
+        data = pd.read_pickle(f"{datafile}-cache.gz")
+    else:
+        data = pd.read_csv(datafile, nrows=nrows)
 
-    data.drop_duplicates(subset=["Text"], inplace=True)
-    data.dropna(inplace=True)
+        data.drop_duplicates(subset=["Text"], inplace=True)
+        data.dropna(inplace=True)
 
-    cleaned_text = []
-    for text in data["Text"]:
-        cleaned_text.append(clean_text(text))
+        cleaned_text = []
+        for text in data["Text"]:
+            cleaned_text.append(clean_text(text))
 
-    cleaned_summary = []
-    for summary in data["Summary"]:
-        cleaned_summary.append(clean_summary(summary))
+        cleaned_summary = []
+        for summary in data["Summary"]:
+            cleaned_summary.append(clean_summary(summary))
 
-    data["cleaned_text"] = cleaned_text
-    data["cleaned_summary"] = cleaned_summary
+        data["cleaned_text"] = cleaned_text
+        data["cleaned_summary"] = cleaned_summary
 
-    # NOTE(bora): Drop any rows with empty values
-    data["cleaned_summary"].replace("", np.nan, inplace=True)
-    data.dropna(inplace=True)
+        # NOTE(bora): Drop any rows with empty values
+        data["cleaned_summary"].replace("", np.nan, inplace=True)
+        data.dropna(inplace=True)
 
-    # NOTE(bora): Mark start and end of each summary
-    data["cleaned_summary"] = data["cleaned_summary"].apply(lambda x: f"__START__ {x} __END__")
+        # NOTE(bora): Mark start and end of each summary
+        data["cleaned_summary"] = data["cleaned_summary"].apply(lambda x: f"__START__ {x} __END__")
+
+        print("Saving preprocessed data to cache file")
+        data.to_pickle(f"{datafile}-cache.gz")
 
     if verbose:
         for i in range(5):
--- code/sum_abstractive.py
+++ code/sum_abstractive.py
@@ -1,8 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import os
-import sys
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # NOTE(bora): Uncomment this line to force CPU mode
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
@@ -14,26 +10,28 @@ import preprocess
 if __name__ == "__main__":
     DATA_DIR = "../data/food_reviews"
 
-    # TODO(bora): Memory hatası alıyorum sürekli, çözemedim bir türlü.
-    # Her şey çalışıyor ama tam sonucu görecekken hafızam bitiyor her halde anlamadım..
     DATA_SIZE = 100000
     BATCH_SIZE = 128  # NOTE(bora): Most efficient when it is a power of 2
 
-    if 1:
-        data = pd.read_csv(f"{DATA_DIR}/Reviews.csv", nrows=DATA_SIZE)
-        MAX_LEN_TEXT = 80
-        MAX_LEN_SUM = 10
+    data_file = f"{DATA_DIR}/Reviews.csv"
+    MAX_LEN_TEXT = 80
+    MAX_LEN_SUM = 10
 
-        (x_train, y_train, x_val, y_val), (x_tokenizer, y_tokenizer) = \
-            preprocess.prepare_dataset(data, max_len_text=MAX_LEN_TEXT, max_len_sum=MAX_LEN_SUM, verbose=False)
+    (x_train, y_train, x_val, y_val), (x_tokenizer, y_tokenizer) = \
+        preprocess.prepare_dataset(data_file, nrows=DATA_SIZE,
+                                   max_len_text=MAX_LEN_TEXT,
+                                   max_len_sum=MAX_LEN_SUM,
+                                   verbose=False)
 
-        # NOTE(bora): Deep learning kısmı
-        model, parameters = nodetails.define_model(x_tokenizer, y_tokenizer, max_len_text=MAX_LEN_TEXT, max_len_sum=MAX_LEN_SUM)
-        model.summary()
+    # NOTE(bora): Deep learning part
+    model, parameters = nodetails.define_model(x_tokenizer, y_tokenizer,
+                                               max_len_text=MAX_LEN_TEXT,
+                                               max_len_sum=MAX_LEN_SUM)
+    model.summary()
 
-        nodetails.train_model(model, parameters, (x_train, y_train), (x_val, y_val),
-                              batch_size=BATCH_SIZE, verbose=True)
+    nodetails.train_model(model, parameters, (x_train, y_train), (x_val, y_val),
+                          batch_size=BATCH_SIZE, verbose=True)
 
-        print("Hadi be, çalıştı mı harbiden?!")
+    print("Done.")
 
 # END OF sum_abstractive.py
