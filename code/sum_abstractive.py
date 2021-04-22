5d0ce8107830484362b8f3d874260bf0d6d39c04
Bora <boraozdogan99@gmail.com>
2021-04-22 16:06:44 +0300

    It works fine now!

--- code/sum_abstractive.py
+++ code/sum_abstractive.py
@@ -27,31 +27,28 @@ if __name__ == "__main__":
         verbose=True,
         show_histogram=False)
 
-    # NOTE(bora): Deep learning part
-    # model, parameters = define_model(x_tokenizer, y_tokenizer,
-    #                                  max_len_text=MAX_LEN_TEXT,
-    #                                  max_len_sum=MAX_LEN_SUM)
-    # model.summary()
-    #
-    # model = train_model(model,
-    #                     (x_train, y_train), (x_val, y_val),
-    #                     batch_size=BATCH_SIZE,
-    #                     show_graph=True)
-    # model_params = prep_for_inference(model, parameters)
-    #
-    # # NOTE(bora): model_params_prime is not the same with model_params.
-    # # The functions successfully save max lengths and tokens and whatnot but
-    # # cannot save and load the actual models. It is something to do with the
-    # # backend of Attention layer I suppose.
-    # #
-    # # I don't know what the hell is wrong with this..
-    # save_nodetails_model(model_params, f"{MODEL_DIR}/{MODEL_NAME}-{DATA_SIZE}.model", debug_output=True)
-    model_params_prime = load_nodetails_model(f"{MODEL_DIR}/{MODEL_NAME}-{DATA_SIZE}.model", debug_output=True)
+    # NOTE(bora): Deep learning part. Change below if statement to choose
+    # between training or loading the model. Arayüz yazmaya üşendim bunun için :/
+    if 2 + 2 == 5:
+        model, parameters = define_model(x_tokenizer, y_tokenizer,
+                                         max_len_text=MAX_LEN_TEXT,
+                                         max_len_sum=MAX_LEN_SUM)
+        model.summary()
+
+        model = train_model(model,
+                            (x_train, y_train), (x_val, y_val),
+                            batch_size=BATCH_SIZE,
+                            show_graph=True)
+        model_params = prep_for_inference(model, parameters)
+        save_nodetails_model(model_params, f"{MODEL_DIR}/{MODEL_NAME}-{DATA_SIZE}.model", debug_output=True)
+    else:
+        # NOTE(bora): It works fine now! Make sure you put the model to the correct path.
+        model_params = load_nodetails_model(f"{MODEL_DIR}/{MODEL_NAME}-{DATA_SIZE}.model", debug_output=True)
 
     print("LEN X TRAIN", len(x_train))
     print("LEN X VAL", len(x_val))
 
-    test_validation_set(x_val, y_val, model_params_prime,
+    test_validation_set(x_val, y_val, model_params,
                         item_range=(42, 52),
                         debug_output=False)
 
